b1c6448d98bbdac3df458089cbef4d03fe4d4161
GertKadiu <gertikadiu56@gmail.com>
2025-01-27 23:38:55 +0100

    second commit

---
+++ data.js
@@ -0,0 +1,26 @@
+export const carouselData = [
+  {
+    imgSrc: "./Images/website-products-square-spring-24-image3.jpg.png",
+    text: "Glide",
+  },
+  {
+    imgSrc: "./Images/website-best-sellers-square-spring-24.jpg.png",
+    text: "Magnetic core bobbin",
+  },
+  {
+    imgSrc: "./Images/website-spring-fragrance-square-spring-24-image4.jpg.png",
+    text: "Premium stabilizer",
+  },
+  {
+      imgSrc: "./Images/website-bundles-square-spring-24-image2.jpg.png",
+      text: "Tropical Threads",
+    },
+    {
+      imgSrc: "./Images/website-bundles-square-spring-24-image2.jpg (1).png",
+      text: "Soothing Scents",
+    },
+    {
+      imgSrc: "./Images/bouquet-gift-set-1-.jpg.png",
+      text: "Vibrant Vibes",
+    },
+];
